593ade9512c2bc3d4f93b8cc5e4bfb460630141c
Abdessamad Touzani <83804777+AbdessamadTzn@users.noreply.github.com>
2024-12-18 14:29:30 +0100

    This fixes #1

--- mastermind_fonctions/gen_combi_secrets.py
+++ mastermind_fonctions/gen_combi_secrets.py
@@ -1,8 +1,7 @@
 import random 
 
-def generation_combinaisons_secrets() :
-    
-    
+def generation_combinaisons_secrets():
+
     secret_list = [ random.randint(0,5) for leng in range (0,4) ]
     return secret_list
 
