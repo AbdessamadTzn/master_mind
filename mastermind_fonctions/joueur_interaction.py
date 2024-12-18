017853da8dacffced7aa5a363f44966705015000
Abdessamad Touzani <83804777+AbdessamadTzn@users.noreply.github.com>
2024-12-18 14:11:59 +0100

    Update joueur_interaction function

--- mastermind_fonctions/joueur_interaction.py
+++ mastermind_fonctions/joueur_interaction.py
@@ -1,12 +1,21 @@
 def joueur_interaction():
 
+    chiffres_acceptes = [0, 1, 2, 3, 4]
+
     try:
-        combinaison_joueur = int(input("Entrer une combinaison de 4 nombres entiers: "))
+        combinaison_joueur = int(input("Entrer une combinaison de 4 nombres entiers entre 0 et 5: "))
     
         if len(str(combinaison_joueur)) != 4:
 
             print("Tu dois entrer exactement 4 nombres !")
-            return joueur_interaction() #To keep calling function using recursio & do not exit
+            return joueur_interaction()     #To keep calling function using recursio & do not exit
+
+        for x in str(combinaison_joueur):
+            if int(x) not in chiffres_acceptes:
+                print("Tu dois entrer des chiffres compris entre 0 et 5 !")
+                return joueur_interaction()     
+
+
 
         combinaison_joueur = [int(num) for num in str(combinaison_joueur)]
 
@@ -14,7 +23,8 @@ def joueur_interaction():
 
     except ValueError:
         #Catch error if input is not digit !
-        print("Erreur: la combinaison doit etre composée des entiers valide")
+        print("La combinaison doit etre composée des entiers valide")
+        return joueur_interaction()
 
     
 
